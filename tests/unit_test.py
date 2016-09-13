0e2c6af184bd30ced7d2f9ffa0c56d6bd42e0e36
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-09-13 10:41:43 -0600

    Updated unit tests for new Unit implementation

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,8 +1,11 @@
 import unittest
 import copy
 
+import data
+
 import dotdot  # pylint: disable=unused-import
 import leabra
+
 
 
 class UnitTestsAPI(unittest.TestCase):
@@ -70,12 +73,14 @@ class UnitTestsBehavior(unittest.TestCase):
             u.add_excitatory(1.0)
             u.calculate_net_in()
             u.cycle()
-            self.assertTrue(0.75 < u.act <= 0.8)
+            self.assertTrue(0.85 < u.act <= 0.95)
 
-        u.add_excitatory(0.0) # u.act should drop to zero immediately.
-        u.calculate_net_in()
-        u.cycle()
-        self.assertEqual(u.act, 0.0)
+        for _ in range(10):
+            u.add_excitatory(0.0)
+            u.calculate_net_in()
+            u.cycle()
+
+        self.assertTrue(u.act < 0.05)
 
 
     def test_xx1_thr(self):
@@ -83,11 +88,10 @@ class UnitTestsBehavior(unittest.TestCase):
         u_spec = leabra.UnitSpec()
         u_spec.act_thr = 0.25
         u = leabra.Unit(spec=u_spec)
-        self.assertEqual(u_spec.xx1(0.20), 0.0)
-        self.assertTrue(u_spec.xx1(0.30) > 0.0)
-        self.assertTrue(1.0e-10 > u_spec.noisy_xx1(0.20) > 0.0)
-        self.assertTrue(u_spec.noisy_xx1(0.30) > 0.0)
-
+        self.assertEqual(u_spec.xx1(-0.1), 0.0)
+        self.assertTrue(0.0 < u_spec.xx1(0.1))
+        self.assertTrue(0.0 < u_spec.noisy_xx1(-0.1) < 1.0e-10 )
+        self.assertTrue(0.1 < u_spec.noisy_xx1(0.1))
 
 
 
