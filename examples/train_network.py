9f04b37ee409978111c67b2136c1fd7bdf05b415
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2017-10-10 18:38:40 +0200

    fix missing line in train_network.py

--- examples/train_network.py
+++ examples/train_network.py
@@ -8,7 +8,7 @@ def build_network(n_input, n_output, n_hidden):
     # specifications
     unit_spec  = leabra.UnitSpec(adapt_on=True, noisy_act=True)
     layer_spec = leabra.LayerSpec(lay_inhib=True)
-    conn_spec  = leabra.ConnectionSpec(proj='full', rnd_type='uniform',  rnd_mean=0.75, rnd_var=0.2)
+    conn_spec  = leabra.ConnectionSpec(proj='full', lrule='leabra', rnd_type='uniform',  rnd_mean=0.50, rnd_var=0.2)
 
     # input/outputs
     input_layer  = leabra.Layer(n_input, spec=layer_spec, unit_spec=unit_spec, name='input_layer')
@@ -24,6 +24,7 @@ def build_network(n_input, n_output, n_hidden):
         connections.append(hidden_conn)
 
     last_conn  = leabra.Connection(layers[-1],  output_layer, spec=conn_spec)
+    connections.append(last_conn)
     layers.append(output_layer)
 
     network_spec = leabra.NetworkSpec(quarter_size=50)
@@ -46,13 +47,17 @@ def train_network(network, input_pattern, output_pattern):
     network.set_inputs({'input_layer': input_pattern})
     network.set_outputs({'output_layer': output_pattern})
 
-    network.trial()
+    sse = network.trial()
+    print(sse)
     return [unit.act_m for unit in network.layers[-1].units]
 
 
 if __name__ == '__main__':
-    network = build_network(36, 24, 1)
-    print(test_network(network, 18*[1.0, 0.0]))
-    print(train_network(network, 18*[1.0, 0.0], 12*[0.0, 1.0]))
-    print(train_network(network, 18*[1.0, 0.0], 12*[0.0, 1.0]))
-    print(train_network(network, 18*[1.0, 0.0], 12*[0.0, 1.0]))
+
+    network = build_network(4, 4, 1)
+    print(test_network(network, [1.0, 1.0, 0.0, 0.0]))
+
+    for i in range(200):
+        print(train_network(network, [1.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 1.0]))
+
+    print(test_network(network, [1.0, 1.0, 0.0, 0.0]))
